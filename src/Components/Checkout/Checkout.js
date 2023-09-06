3a69ef843fcd48d5829db59db73e74eba4eac01d
1Sarry <saronmeles4@gmail.com>
2023-09-06 18:00:28 +0300

    Finish Check Out

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import "./Checkout.scss"
-
-
+import React from "react";
+import "./Checkout.scss";
+// import Subtotal from "./Subtotal/Subtotal"
 const Checkout = () => {
   return (
-    <div>Checkout</div>
-  )
-}
+    <div className="checkout">
+      <div className="checkout-left">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+        />
+        <h3>Hello,</h3>
+        <h2 className="checkout-title">Your Shopping Basket</h2>
+      </div>
+      <div className="checkout-right">
+        {/* <Subtotal /> */}
+      </div>
+    </div>
+  );
+};
 
-export default Checkout
+export default Checkout;
